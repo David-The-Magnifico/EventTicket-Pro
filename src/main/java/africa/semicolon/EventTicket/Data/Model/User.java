0d78b36c33b856984ccd41913c52fcc0457a7613
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-07-04 12:00:59 +0100

    added enum classes to model directory

---
+++ src/main/java/africa/semicolon/EventTicket/Data/Model/User.java
@@ -0,0 +1,13 @@
+package africa.semicolon.EventTicket.Data.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Users")
+public class User {
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
